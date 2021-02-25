3cee617da01ea5c80f5d3ce082b1a762b169c6d7
Aaron Roller <AaronMichaelRoller@gmail.com>
2021-02-24 20:28:58 -0600

    Add fish abbrs

---
+++ .config/fish/config.fish
@@ -0,0 +1,17 @@
+# Git abbreviations
+abbr -a ga "git add"
+abbr -a gc "git commit"
+abbr -a gp "git push"
+abbr -a gfp "git push --force-with-lease"
+abbr -a gd "git diff"
+abbr -a gst "git status"
+abbr -a gco "git checkout"
+abbr -a gb "git branch"
+
+# Navigation abbreviations
+abbr -a c "clear"
+
+# Miscellaneous
+abbr -a resource "source ~/.config/fish/config.fish"
+abbr -a fishconfig "nvim ~/.config/fish/config.fish"
+
